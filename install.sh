9c9c6a36475d846541302cc56ac8a59cafad4ed5
rd113 <randodad@proton.me>
2026-03-08 04:02:41 +0000

    Fix symbolic links

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-# Symlink dotfiles
-DOTFILES_DIR="$HOME/.dotfiles"
+DOTFILES_DIR="$HOME/.config/coderv2/dotfiles"
 
 ln -sf "$DOTFILES_DIR/.bashrc" "$HOME/.bashrc"
-ln -sf "$DOTFILES_DIR/.vimrc" "$HOME/.vimrc"
-ln -sf "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
+#ln -sf "$DOTFILES_DIR/.vimrc" "$HOME/.vimrc"
+#ln -sf "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
 # Add more as needed
+
