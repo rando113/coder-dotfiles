f59d58336bf97e2d63ee364a6bd74f7fd1e0e51e
rd113 <randodad@proton.me>
2026-04-26 22:34:31 +0000

    Fix Chrome issues

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@ set -e
 DOTFILES_DIR="$HOME/.config/coderv2/dotfiles"
 
 ln -sf "$DOTFILES_DIR/.bashrc" "$HOME/.bashrc"
+
+# Delete leftovers that prevent Chrome from launching
+rm -f ~/.config/google-chrome/SingletonLock || echo "Good! No Chrome SingletonLock"
+rm -f ~/.config/google-chrome/SingletonSocket || echo "Good! No Chrome SingletonSocket"
+rm -f ~/.config/google-chrome/SingletonCookie || echo "Good! No Chrome SingletonCookie"
+
 #ln -sf "$DOTFILES_DIR/.vimrc" "$HOME/.vimrc"
 #ln -sf "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
 # Add more as needed
